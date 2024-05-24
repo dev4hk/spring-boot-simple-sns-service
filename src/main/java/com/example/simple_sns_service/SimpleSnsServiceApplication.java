f339d500650abc03f92ecb9b70f1aca1d55e73d5
Hoon <hoon0661@gmail.com>
2024-05-24 15:52:45 -0400

    initial commit

---
+++ src/main/java/com/example/simple_sns_service/SimpleSnsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_sns_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSnsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSnsServiceApplication.class, args);
+	}
+
+}
