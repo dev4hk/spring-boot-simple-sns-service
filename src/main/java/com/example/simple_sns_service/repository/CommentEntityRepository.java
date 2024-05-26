c6ad13b1e435cbcf7ec39267b8505101d179b46a
Hoon <hoon0661@gmail.com>
2024-05-26 16:32:07 -0400

    added method - delete likes & comments upon deletion of post, used native query delete each entity in one update query by not retrieving entities into persistence context and update, which is 2 queries for each deletion

--- src/main/java/com/example/simple_sns_service/repository/CommentEntityRepository.java
+++ src/main/java/com/example/simple_sns_service/repository/CommentEntityRepository.java
@@ -5,7 +5,16 @@ import com.example.simple_sns_service.model.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Integer> {
     Page<CommentEntity> findAllByPost(PostEntity postEntity, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE CommentEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(@Param("post")PostEntity postEntity);
 }
